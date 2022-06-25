f23f323aba1be1917873083def1d7cb69482c5ac
winner021 <90254653+winner021@users.noreply.github.com>
2022-06-25 21:47:37 +0530

    changing api key

--- src/helperPages/Black.js
+++ src/helperPages/Black.js
@@ -2,13 +2,15 @@ import React from 'react'
 import "../styling/css/style.css"
 import RecentlyPlayed from '../component/RecentlyPlayed'
 import GenreListFour from '../component/GenreListFour'
+import GenreListone from '../component/GenreListone'
+
 
 const Black=()=>{
     return(
         <div className='Black_Container'>
             <h2>recommendation</h2>
             <div className='genre_Container'>
-            <GenreListFour/>
+            <GenreListone/>
             </div>
       
         </div>
